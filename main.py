536d1a411d2cb19a4362edaa3522a4a03c9d96ed
pdpate22 <pdpate22@ncsu.edu>
2025-05-26 13:48:01 -0400

    Made progress with dynamic date range. It enables YTD stock info! Next steps are to rethink about what data to include and format the data into a plot / graph

--- main.py
+++ main.py
@@ -9,81 +9,75 @@ api_key = os.getenv("ALPHAVTANGE_API_KEY")
 base_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-symbol = "AAPL"
-symbol = input("Enter a symbol: ")
+
+symbol = "TSLA"
+# symbol = input("Enter a symbol: ")
 url = f"{base_url}{symbol}&apikey={api_key}"
 r = requests.get(url)
 data = r.json()
 
-yesterday = datetime.date.today() - datetime.timedelta(days=1)
-yesterday = yesterday.strftime("%Y-%m-%d")
+# temp reader for offline testing
+with open("formatted.json", "r") as file:
+    data = json.load(file)
 
+# monthly_data = data['Monthly Time Series']
+monthly_data = data
+sorted_dates = sorted(monthly_data.keys())
+curr_year = str(datetime.datetime.now().year)
+
+# testing a input feature
+year = input("Enter a year: ")
+curr_year = year
+
+# writes api data into json
 with open("formatted.json", "w") as file:
-    daily_data = data['Monthly Time Series'][yesterday]
-#     for line, price in daily_data.items():
-#         print(f"{line}: {price}", file=file)
-    print(json.dumps(data, indent=2), file=file)
+    print(json.dumps(monthly_data, indent=2), file=file)
 
+# reads json data
+with open("formatted.json", "r") as file:
+    ytd_month = []
+    ytd_data = []
+
+    # adds month for ytd
+    for month in sorted_dates:
+        if month.startswith(curr_year):
+            ytd_month.append(month)
+
+    # adds data for ytd
+    for month in ytd_month:
+        ytd_data.append({
+            "month": month,
+            "price": monthly_data[month]["4. close"]
+        })
+
+    # writes month and price data into txt
+    with open("output.txt", "w") as file:
+        print(f"{symbol} {curr_year} YTD Stock Data:\n", file=file)
+
+        for entry in ytd_data:
+            print(f"{entry['month']}: ${float(entry['price']):,.2f}", file=file)
+                
+
+'''
+# print json data into txt
 with open("formatted.json", "r") as file:
     data = json.load(file)
     month = data['Monthly Time Series']
     left = []
     height = []
     starting_month = ['2024-06-28']
-    
-    with open("output.txt", "w") as file:
-        for month in data['Monthly Time Series']:
-            if month == '2023-06-30':
-                break
-            if month == '2024-06-28' or month == "2024-05-31" or month == "2024-04-30" or month == "2024-03-28" or month == "2024-02-29" or month == "2024-01-31" or month == "2023-12-29" or month == "2023-11-30" or month == "2023-10-31" or month == "2023-09-29" or month == "2023-08-31" or month == "2023-07-31":
-                monthly_data = [data['Monthly Time Series'][month]]
-                if "-01-" in month:
-                        print(f"\nJan:", file=file)
-                        left.append("Jan")
-                if "-02-" in month:
-                        print(f"\nFeb:", file=file)
-                        left.append("Feb")
-                if "-03-" in month:
-                        print(f"\nMar:", file=file)
-                        left.append("Mar")
-                if "-04-" in month:
-                        print(f"\nApr:", file=file)
-                        left.append("Apr")
-                if "-05-" in month:
-                        print(f"\nMay:", file=file)
-                        left.append("May")
-                if "-06-" in month:
-                        print(f"\nJun:", file=file)
-                        left.append("Jun")
-                if "-07-" in month:
-                        print(f"\nJul:", file=file)
-                        left.append("Jul")
-                if "-08-" in month:
-                        print(f"\nAug:", file=file)
-                        left.append("Aug")
-                if "-09-" in month:
-                        print(f"\nSep:", file=file)
-                        left.append("Sep")
-                if "-10-" in month:
-                        print(f"\nOct:", file=file)
-                        left.append("Oct")
-                if "-11-" in month:
-                        print(f"\nNov:", file=file)
-                        left.append("Nov")
-                if "-12-" in month:
-                        print(f"\nDec:", file=file)
-                        left.append("Dec")
-                
-                for info in monthly_data:
-                    for field, information in info.items():
-                        if field == "2. high":
-                            print(f"1. High: ${float(information):,.2f}", file=file)
-                            height.append(float(information))
-                        if field == "3. low":
-                            print(f"2. Low: ${float(information):,.2f}", file=file)
-                        if field == "5. volume":
-                            print(f"3. Volume: {int(information):,}", file=file)
+      
+    for info in monthly_data:
+        for field, information in info.items():
+            if field == "2. high":
+                print(f"1. High: ${float(information):,.2f}", file=file)
+                height.append(float(information))
+            if field == "3. low":
+                print(f"2. Low: ${float(information):,.2f}", file=file)
+            if field == "5. volume":
+                print(f"3. Volume: {int(information):,}", file=file)
 
+    # graph
     plt.plot(left, height, linestyle='solid', linewidth = 2,
          marker='o', markerfacecolor='black', markersize=6)
 
@@ -105,3 +99,5 @@ with open("formatted.json", "r") as file:
     plt.xlim(1, 12)
     plt.title(f"{symbol}")
     plt.show()
+
+'''
