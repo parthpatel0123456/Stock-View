48b7dd9f14cb072f954385e784b288979e7f28f9
pdpate22 <pdpate22@ncsu.edu>
2025-06-25 18:27:16 -0400

    simplified flask code

--- website/__init__.py
+++ website/__init__.py
@@ -14,13 +14,9 @@ def create_app():
 
     CORS(app)
 
-
     from .views import views
-    from .auth import auth
-
+    
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
 
     return app
     
